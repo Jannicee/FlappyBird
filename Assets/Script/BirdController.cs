d15a99a9f00747364ce547c619e05b0dbe2dd787
Jannicee <jsandiakurniawan@gmail.com>
2025-12-12 02:33:34 +0700

    Add Movement and Die Condition
    Movement and Die Condition to Player

--- Assets/Script/BirdController.cs
+++ Assets/Script/BirdController.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class BirdController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D _rb2d;
+    private float _flapforce = 5f;
+    private bool _isAlive = true;
+    [SerializeField] private float _yBounds = -5f;
+    [SerializeField] private float _topBounds = 6f;
+
     void Start()
     {
-        
+        _rb2d = GetComponent<Rigidbody2D>();
     }
 
+   
+    public void Flap()
+    {
+        if (_isAlive)
+        {
+            
+           //Debug.Log("Flap!");
+            _rb2d.velocity = Vector2.zero;
+            _rb2d.velocity = Vector2.up * _flapforce;
+   
+        }
+    }
+
+    public void Die()
+    {
+        if (!_isAlive) return;
+        _isAlive = false;
+        Debug.Log("Die!");
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Flap();
+        }
+        if (_isAlive && transform.position.y < _yBounds || transform.position.y > _topBounds)
+        {
+            Die();
+        }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        Die();
+    }
+   
 }
